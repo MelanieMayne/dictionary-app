85f20ed41e6b88ec340befa7779625ee36603bcf
MelanieMayne <103015325+MelanieMayne@users.noreply.github.com>
2023-02-13 18:51:02 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="App-header">
+        </header>
+        <main>
+          Dictionary App Coming Soon...
+        </main>
+      </div>  
+    </div>
+  );
+}
+
+
