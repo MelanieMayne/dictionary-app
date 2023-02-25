207bc77a9b37b07d6044720c8bc816f34a693789
MelanieMayne <103015325+MelanieMayne@users.noreply.github.com>
2023-02-24 18:03:46 -0800

    refactored code to have a default word searched and displayed

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,33 +3,47 @@ import Results from "./Results";
 import "./Dictionary.css";
 import axios from "axios";
 
-export default function Dictionary() {
-    let [searchedWord, setSearchedWord] = useState("");
+export default function Dictionary(props) {
+    let [searchedWord, setSearchedWord] = useState(props.defaultSearchedWord);
     let [results, setResults] = useState();  
+    let [loaded, setLoaded] = useState(false);
 
     function handleWord(event) {
         setSearchedWord(event.target.value);
     }
 
-    function handleSearch(event) {
-        event.preventDefault();
-
+    function search() {
         let ApiKey = "440461d4fbdf3d442aeb4ff32t4abo8a";
         let ApiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${searchedWord}&key=${ApiKey}`;
         axios.get(ApiUrl).then(handleResponse);
     }
 
+    function handleSubmit(event) {
+        event.preventDefault();
+        search();
+    }
+
     function handleResponse(response) {
        setResults(response.data);
     } 
+     
+    function load() {
+        setLoaded(true);
+        search();
+    }
 
-    return (
-        <div className="Dictionary">
-            <form onSubmit={handleSearch}>
-                <input type="search" placeholder="Lookup Word..." className="search-bar" onChange={handleWord}></input>
-                <input type="submit" value="Search" className="search-button"></input>
-            </form>
-            <Results results={results} />
-        </div>
-    );
+    if (loaded) {
+        return (
+            <div className="Dictionary">
+                <form onSubmit={handleSubmit}>
+                    <input type="search" placeholder="Lookup Word..." className="search-bar" onChange={handleWord}></input>
+                    <input type="submit" value="Search" className="search-button"></input>
+                </form>
+                <Results results={results} />
+            </div>
+        );
+    } else {
+        load();
+    }
+   
 }
