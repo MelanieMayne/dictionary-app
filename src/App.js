fa1d430badb0ce6b4504c3351f7214e2f684c6d6
MelanieMayne <103015325+MelanieMayne@users.noreply.github.com>
2023-02-24 18:16:11 -0800

    added footer and styling for footer

--- src/App.js
+++ src/App.js
@@ -13,6 +13,11 @@ export default function App() {
         <main>
           <Dictionary defaultSearchedWord="Novel"/>
         </main>
+        <footer>
+          <p>
+            This app is <a href="https://github.com/MelanieMayne/dictionary-app" target="_blank" rel="noreferrer">Open-Source Coded</a> by <a href="https://cosmic-daffodil-494003.netlify.app/" target="_blank" rel="noreferrer">Melanie Richardson</a> and hosted on <a href="https://app.netlify.com/sites/starlit-duckanoo-6a3aa1/deploys" target="_blank" rel="noreferrer">Netlify</a>
+          </p>
+        </footer>
       </div>  
     </div>
   );
